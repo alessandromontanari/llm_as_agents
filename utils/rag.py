87397e1461a681795108b9393c3967221cfdb078
Alessandro Montanari <a.montanari.05@gmail.com>
2025-02-07 14:35:12 +0100

    utils for rag graph builder

---
+++ utils/rag.py
@@ -0,0 +1,54 @@
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+def retrieve(state: State, vector_store_titles, vector_store_keywords):
+
+    retrieved_docs_titles = vector_store_titles.similarity_search_with_score(state["question"], k=5)
+    retrieved_docs_keywords = vector_store_keywords.similarity_search_with_score(state["question"], k=5)
+
+    title_scores = {doc.metadata["document"]: score for doc, score in retrieved_docs_titles}
+    keyword_scores = {doc.metadata["document"]: score for doc, score in retrieved_docs_keywords}
+
+    title_pg_contents = {doc.metadata["document"]: doc.page_content for doc, _ in retrieved_docs_titles}
+    keyword_pg_contents = {doc.metadata["document"]: doc.page_content for doc, _ in retrieved_docs_keywords}
+
+    common_docs = []
+    avg_scores = []
+    for doc_id in title_scores.keys() & keyword_scores.keys():  # Intersection of doc IDs
+        avg_score = (title_scores[doc_id] + keyword_scores[doc_id]) / 2.  # Averaging scores
+        common_docs.append(doc_id)
+        avg_scores.append(avg_score)
+    # Sort by best (lowest) average score
+    # TODO: check if the lowest score is actually the best
+    avg_scores.sort()
+    sorted_indices = sorted(range(len(avg_scores)), key=lambda ii: avg_scores[ii])
+    common_docs = [common_docs[ii] for ii in sorted_indices]
+
+    output_documents = []
+
+    for ii, doc_id in enumerate(common_docs):
+        output_documents.append(
+            (Document(
+                id='', metadata={'document': doc_id},
+                # page_content=keyword_pg_contents.get(doc_id)
+                page_content=title_pg_contents.get(doc_id)
+            ),
+             avg_scores[ii])
+        )
+
+    # TODO: this may be a problem because the output_documents are not recognized
+    #  by the generate even though they should be correct
+
+    return {"context": output_documents}
+
+def generate(state: State, prompt, llm):
+    docs_content = "\n\n".join(doc[0].page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
