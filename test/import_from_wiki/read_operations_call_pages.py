796cdf3e1428c1edacfad092ea17418e85d7f7bc
Alessandro Montanari <a.montanari.05@gmail.com>
2025-03-20 10:22:23 +0100

    unit test on reading OPS call pages successfull

---
+++ test/import_from_wiki/read_operations_call_pages.py
@@ -0,0 +1,15 @@
+import unittest
+from utils.read_wiki_pages import process_meeting_minutes_ops_calls
+
+
+class TestReadOPSCallPages(unittest.TestCase):
+    def test_read_ops_calls_pages(self):
+
+        file_path = './data/hess_pages/operations_call__25_04_2024.txt'  # Replace with your file path
+        structured_data, hess_urls = process_meeting_minutes_ops_calls(file_path)
+
+        for title, content in structured_data.items():
+            print(f"{title}:\n{content}\n")
+
+if __name__ == '__main__':
+    unittest.main()
